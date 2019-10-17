0d1c731278be4aa811ddacb1e08dd82308e4b0c6
Barış Topcu <baaristopcu@gmail.com>
2019-10-18 01:14:44 +0300

    Caching added for photos.

--- BisLeagues.Presentation/Controllers/ImageController.cs
+++ BisLeagues.Presentation/Controllers/ImageController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using BisLeagues.Core.Interfaces;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.FileProviders;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
@@ -15,35 +17,44 @@ namespace BisLeagues.Presentation.Controllers
 {
     public class ImageController : Controller
     {
-        public ImageController(IHostingEnvironment env)
+
+        private readonly IFileProvider _fileProvider;
+        private readonly IDistributedCache _cache;
+
+        public ImageController(IHostingEnvironment env, IDistributedCache cache)
         {
             _fileProvider = env.WebRootFileProvider;
+            _cache = cache;
         }
 
 
-        private readonly IFileProvider _fileProvider;
 
         [Route("/images/{width}/{height}/{*url}")]
-        public IActionResult Resize(string url, int width = 0, int height = 0)
+        public async Task<IActionResult> Resize(string url, int width = 0, int height = 0)
         {
 
             if (width < 0 || height < 0) { return BadRequest(); }
-
-            var imagePath = PathString.FromUriComponent("/" + url);
-            var fileInfo = _fileProvider.GetFileInfo(imagePath);
-            if (!fileInfo.Exists) { return NotFound(); }
-
-            var outputStream = new MemoryStream();
-            using (var inputStream = fileInfo.CreateReadStream())
-            using (Image<Rgba32> image = Image.Load(inputStream))
+            var key = $"/{width}/{height}/{url}";
+            var data = await _cache.GetAsync(key);
+            if (data == null)
             {
-                image.Mutate(ctx => ctx.Resize(width,height));
-                image.SaveAsJpeg(outputStream);
+                var imagePath = PathString.FromUriComponent("/" + url);
+                var fileInfo = _fileProvider.GetFileInfo(imagePath);
+                if (!fileInfo.Exists) { return NotFound(); }
+                using (var outputStream = new MemoryStream())
+                {
+                    using (var inputStream = fileInfo.CreateReadStream())
+                    using (Image<Rgba32> image = Image.Load(inputStream))
+                    {
+                        image.Mutate(ctx => ctx.Resize(width, height));
+                        image.SaveAsJpeg(outputStream);
+                    }
+                    data = outputStream.ToArray();
+                }
+                await _cache.SetAsync(key, data, new DistributedCacheEntryOptions());
+
             }
-
-            outputStream.Seek(0, SeekOrigin.Begin);
-
-            return File(outputStream, "image/jpg");
+            return File(data, "image/jpg");
         }
 
     }
